ffc3902051687e58216cc1867248cfbfce60b459
Fahad Jamal <fmj213@stern.nyu.edu>
2015-07-10 21:57:36 +0000

    reinitialize project

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,45 @@
+class PlacesController < ApplicationController
+    before_action :authenticate_user!, :only => [:new, :create]
+	def index
+		@places = Place.all
+	end
+
+	def new
+		@place = Place.new
+    end
+
+    def create
+     current_user.places.create(place_params)
+     redirect_to root_path
+    end
+
+    def destroy
+      @place = Place.find(params[:id])
+      @place.destroy
+      redirect_to root_path
+    end
+
+    def show
+      @place = Place.find(params[:id])
+    end
+
+    def edit
+      @place = Place.find(params[:id])
+    end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.update_attributes(place_params)
+        redirect_to root_path
+    end
+
+
+
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
+end
