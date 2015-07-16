fb785616561cfa7dc57c75ecde70b8887a4a5fa3
Fahad Jamal <fmj213@stern.nyu.edu>
2015-07-16 18:19:34 +0000

    Google Maps API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,45 +1,64 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, :only => [:new, :create]
+    before_action :authenticate_user!, :only => [:new, :create, :update, :destroy]
+    before_action :authorize, :only => [:edit]
 	def index
 		@places = Place.all
 	end
 
 	def new
 		@place = Place.new
-    end
+  end
 
-    def create
-     current_user.places.create(place_params)
-     redirect_to root_path
-    end
-
-    def destroy
-      @place = Place.find(params[:id])
-      @place.destroy
+  def create
+     @place = current_user.places.create(place_params)
+    if @place.valid?
       redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+  end
     end
 
-    def show
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
+
+  def show
       @place = Place.find(params[:id])
-    end
+      @comment = Comment.new
+  end
 
-    def edit
+  def edit
       @place = Place.find(params[:id])
+  end
+
+  def update
+     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render :text => 'Not Allowed', :status => :forbidden
     end
 
-    def update
-        @place = Place.find(params[:id])
-        @place.update_attributes(place_params)
-        redirect_to root_path
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, :status => :unprocessable_entity
     end
-
-
-
+  end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
+  end
+
+  def authorize
+    p @place = Place.find(params[:id])
+    unless @place.user == current_user
+    redirect_to root_path
+    end
   end
 
 end
